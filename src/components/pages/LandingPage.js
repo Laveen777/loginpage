8e9cc306f68e8efe7add8c3bd40369278e33a6d1
Laveen777 <120643512+Laveen777@users.noreply.github.com>
2023-04-18 11:38:38 +0530

    Add files via upload

---
+++ src/components/pages/LandingPage.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../../App.css'
+import BackgroundImage from '../../assets/images/newPage.jpg'
+
+export default function LandingPage() {
+    return (
+        <header style={ HeaderStyle }>
+            <p className="main-para">NewPage</p>
+            <p className="main-para1 text-center">Tomorrow's Digital Health Solutions</p>
+            <h1 className="main-title text-center">login / register page</h1>
+            
+            <div className="buttons text-center">
+                <Link to="/login">
+                    <button className="primary-button">log in</button>
+                </Link>
+                <Link to="/register">
+                    <button className="primary-button" id="reg_btn"><span>register </span></button>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    background: `url(${BackgroundImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+}
